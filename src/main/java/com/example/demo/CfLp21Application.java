d41da6e53b92629070d5d25a5f423c30543d06e2
Arturo Campos <i202310006@cibertec.edu.pe>
2024-03-09 23:31:47 -0500

    examen lp2 laboratorio final

---
+++ src/main/java/com/example/demo/CfLp21Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfLp21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfLp21Application.class, args);
+	}
+
+}
